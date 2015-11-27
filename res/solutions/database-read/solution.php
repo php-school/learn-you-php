e28ccf759777618506bf53af1543a88aee2db165
Aydin Hassan <aydin@hotmail.co.uk>
2015-11-27 00:21:42 +0000

    Require updating values, harder query

--- res/solutions/database-read/solution.php
+++ res/solutions/database-read/solution.php
@@ -1,10 +1,10 @@
 <?php
 
 $db = new \PDO($argv[1]);
-
-$users = $db->query('SELECT * FROM users');
-
+$users = $db->query('SELECT * FROM users WHERE age > 30');
 foreach ($users as $user) {
-    var_dump($user);
-    echo "\n";
+    echo "User: {$user['name']} Age: {$user['age']} Sex: {$user['gender']}\n";
 }
+$nameToUpdate = $argv[2];
+$stmt = $db->prepare('UPDATE users SET name = :newName WHERE name = :oldName');
+$stmt->execute(array(':newName' => 'David Attenborough', ':oldName' => $nameToUpdate));
--- src/Exercise/DatabaseRead.php
+++ src/Exercise/DatabaseRead.php
@@ -3,20 +3,35 @@
 namespace PhpSchool\LearnYouPhp\Exercise;
 
 use Faker\Generator;
+use PDO;
+use PhpSchool\PhpWorkshop\CodeInsertion as CI;
+use PhpSchool\PhpWorkshop\Exercise\AbstractExercise;
 use PhpSchool\PhpWorkshop\Exercise\ExerciseInterface;
-use PhpSchool\PhpWorkshop\ExerciseCheck\FunctionRequirementsExerciseCheck;
+use PhpSchool\PhpWorkshop\Exercise\SubmissionPatchable;
+use PhpSchool\PhpWorkshop\Exercise\TemporaryDirectoryTrait;
+use PhpSchool\PhpWorkshop\ExerciseCheck\SelfCheck;
 use PhpSchool\PhpWorkshop\ExerciseCheck\StdOutExerciseCheck;
+use PhpSchool\PhpWorkshop\Patch;
+use PhpSchool\PhpWorkshop\Result\Failure;
+use PhpSchool\PhpWorkshop\Result\ResultInterface;
+use PhpSchool\PhpWorkshop\Result\Success;
+use PhpSchool\PhpWorkshop\SubmissionPatch;
 use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Class DatabaseRead
  * @package PhpSchool\LearnYouPhp\Exercise
  * @author Michael Woodawrd <mikeymike.mw@gmail.com>
+ * @author Aydin Hassan <aydin@hotmail.co.uk>
  */
-class DatabaseRead implements
+class DatabaseRead extends AbstractExercise implements
     ExerciseInterface,
-    StdOutExerciseCheck
+    StdOutExerciseCheck,
+    SelfCheck,
+    SubmissionPatchable
 {
+    use TemporaryDirectoryTrait;
+    
     /**
      * @var Filesystem
      */
@@ -28,9 +43,14 @@ class DatabaseRead implements
     private $faker;
 
     /**
-     * @var \PDO
+     * @var PDO
      */
     private $db;
+
+    /**
+     * @var int
+     */
+    private $randomId;
 
     /**
      * @param Generator $faker
@@ -58,22 +78,6 @@ class DatabaseRead implements
     }
 
     /**
-     * @return string
-     */
-    public function getSolution()
-    {
-        return __DIR__ . '/../../res/solutions/database-read/solution.php';
-    }
-
-    /**
-     * @return string
-     */
-    public function getProblem()
-    {
-        return __DIR__ . '/../../res/problems/database-read/problem.md';
-    }
-
-    /**
      * @return array
      */
     public function getArgs()
@@ -81,12 +85,13 @@ class DatabaseRead implements
         $this->filesystem->mkdir($this->getTemporaryPath());
 
         $dbPath   = sprintf('sqlite:%s/db.sqlite', $this->getTemporaryPath());
-        $this->db = new \PDO($dbPath);
-        $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->db = new PDO($dbPath);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $this->populateDb();
+        $names          = $this->populateDb();
+        $this->randomId = array_rand($names);
 
-        return [$dbPath];
+        return [$dbPath, $names[$this->randomId]];
     }
 
     /**
@@ -98,43 +103,55 @@ class DatabaseRead implements
     }
 
     /**
-     * @return string
-     */
-    private function getTemporaryPath()
-    {
-        return sprintf('%s/%s', realpath(sys_get_temp_dir()), str_replace('\\', '_', __CLASS__));
-    }
-
-    /**
      * Populate database with fake data
      */
     private function populateDb()
     {
-        $this->db->exec(
-            'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT)'
-        );
+        $this->db
+            ->exec('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT)');
+        $stmt = $this->db->prepare('INSERT into users (name, age, gender) VALUES (:name, :age, :gender)');
 
-        $stmt   = $this->db->prepare('INSERT into users (name, age, gender) VALUES (:name, :age, :gender)');
-
+        $names = [];
         foreach (range(0, rand(5, 15)) as $id) {
-            $name  = $this->faker->name;
-            $age   = rand(18, 90);
+            $name   = $this->faker->name;
+            $age    = rand(18, 90);
             $gender = rand(0, 100) % 2 ? 'male' : 'female';
 
-            $stmt->bindParam('name', $name, \PDO::PARAM_STR);
-            $stmt->bindParam('age', $age, \PDO::PARAM_INT);
-            $stmt->bindParam('gender', $gender, \PDO::PARAM_STR);
-
-            $stmt->execute();
+            $stmt->execute([':name' => $name, ':age' => $age, ':gender' => $gender]);
+            $id = $this->db->lastInsertId();
+            $names[$id] = $name;
         }
+        return $names;
+    }
 
-        $users = $this->db->query('SELECT * FROM users');
+    /**
+     * @param string $fileName
+     * @return ResultInterface
+     */
+    public function check($fileName)
+    {
+        $sql = 'SELECT name FROM users WHERE id = :id';
+        $stmt = $this->db->prepare($sql);
 
-        $test = '';
-        foreach ($users as $user) {
-            $test .= sprintf('Name: %s, Age: %s, Gender: %s', $user['name'], $user['age'], $user['gender']);
-            $test .= "\n";
+        $stmt->execute([':id' => $this->randomId]);
+        $result = $stmt->fetchColumn();
+        
+        if ($result !== 'David Attenborough') {
+            return Failure::fromNameAndReason('Update Database', 'The requested row was not updated');
         }
+        
+        return new Success('Update Database');
+    }
 
+    /**
+     * @return Patch
+     */
+    public function getPatch()
+    {
+        return (new Patch)
+            //hack to keep an un-modified version of the db for solution and submission checking
+            //otherwise the time the db is passed to submission, it is modified.    
+            ->withInsertion(new CI(CI::TYPE_BEFORE, '$___db = substr($argv[1], 7); copy($___db, "originalDb");'))
+            ->withInsertion(new CI(CI::TYPE_AFTER, 'rename("originalDb", $___db);'));
     }
 }
